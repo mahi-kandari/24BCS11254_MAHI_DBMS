9c5ade9e2768d9d4d1ca2af4a11700f320c69d44
Mahi Kandari <mahikandari.ntl@gmail.com>
2026-07-23 15:52:52 +0530

    ex 3.1

---
+++ Experiments/Experiment3/Exp3.1/Count using Case.sql
@@ -0,0 +1,7 @@
+/* Write a query to count the number of students across departments who has scored more than 80 marks.*/
+
+
+select department, 
+count(case when Marks>80 then 1 else NULL end ) as Dept_HighScore_Count
+from student 
+group by department;
